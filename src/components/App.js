65da9ebe2350c5904c94563af54d00be5700ab4d
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-10-05 05:38:08 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
-import MoviesPage from "./MoviesPage";
+import MoviesPage from "./MoviePage";
 
 function App() {
-  const [movies, setMovies] = useState({
+  const [movies/*, setMovies*/] = useState({
     1: { id: 1, title: "A River Runs Through It" },
     2: { id: 2, title: "Se7en" },
     3: { id: 3, title: "Inception" },
---
+++ src/components/MoviePage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useRouteMatch } from "react-router-dom";
+import { Route } from "react-router-dom";
+import MovieShow from "./MovieShow";
+import MoviesList from "./MovieList";
+
+function MoviesPage({ movies }) {
+  const match = useRouteMatch()
+  console.log(match)
+  return (
+    <div>
+      <MoviesList movies={movies} />
+      <Route path={`${match.url}/:movie`}/>
+      <MovieShow movies={movies}/>
+    </div>
+  );
+}
+export default MoviesPage;
